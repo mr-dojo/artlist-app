964a397598c2ab824f35f4d71534b62d2cfc5ab4
Carlo Costantino <costantinoart@gmail.com>
2020-02-24 16:04:55 -1000

    Removed login and signup links

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.css";
 
 class Nav extends React.Component {
   render() {
     return (
       <nav role="navigation">
-        <Link to={`/login`}>login</Link>
-        <Link to={`/signup`}>signup</Link>
         <Link to={`/add`}>add item</Link>
         <Link to={`/view`}>view</Link>
       </nav>
