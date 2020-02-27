764a1109ad231c21ae50ca89129f2a3443bef3cb
Carlo Costantino <costantinoart@gmail.com>
2020-02-27 10:25:52 -1000

    Implimented dynamic view item list rendering

--- src/ViewPage/ViewPage.js
+++ src/ViewPage/ViewPage.js
@@ -1,67 +1,72 @@
 import React from "react";
 import "./ViewPage.css";
 
-// const newStaticItems = [
-//   {
-//     title: "Lahaina Fish",
-//     medium: "oil",
-//     description: "a colorful green and purple fish",
-//     location: "Maui Hands Lahaina",
-//     availability: "Complicated",
-//     price: "2500"
-//   },
-//   {
-//     title: "Honu",
-//     medium: "mixed media",
-//     description: "a turtle in a sea of blue",
-//     availability: "Unavailable",
-//     location: "somewhere in Canada"
-//   },
-//   {
-//     title: "Dancing Clown",
-//     medium: "acrylic",
-//     description: "a scary clown",
-//     location: "Maui Hands Lahaina",
-//     availability: "Complicated",
-//     price: "2500"
-//   },
-//   {
-//     title: "Makawao Fish",
-//     medium: "mixed media",
-//     description: "A fish of the mountain",
-//     availability: "Unavailable",
-//     size: "20x40"
-//   },
-//   {
-//     title: "Haleakala",
-//     medium: "acrylic",
-//     description: "A giant volcano that looks like a mountain",
-//     size: "20x16",
-//     availability: "Available",
-//     price: "3000"
-//   }
-// ];
+const staticViewItems = [
+  {
+    title: "Lahaina Fish",
+    medium: "oil",
+    description: "a colorful green and purple fish",
+    location: "Maui Hands Lahaina",
+    availability: "Complicated",
+    price: "2500"
+  },
+  {
+    title: "Honu",
+    medium: "mixed media",
+    description: "a turtle in a sea of blue",
+    availability: "Unavailable",
+    location: "somewhere in Canada"
+  },
+  {
+    title: "Dancing Clown",
+    medium: "acrylic",
+    description: "a scary clown",
+    location: "Maui Hands Lahaina",
+    availability: "Complicated",
+    price: "2500"
+  },
+  {
+    title: "Makawao Fish",
+    medium: "mixed media",
+    description: "A fish of the mountain",
+    availability: "Unavailable",
+    size: "20x40"
+  },
+  {
+    title: "Haleakala",
+    medium: "acrylic",
+    description: "A giant volcano that looks like a mountain",
+    size: "20x16",
+    availability: "Available",
+    price: "3000"
+  }
+];
 
-const newStaticItem = {
-  title: "Haleakala",
+const activeFilters = {
   medium: "acrylic",
-  description: "A giant volcano that looks like a mountain",
-  size: "20x16",
-  availability: "Available",
-  price: "3000"
+  availability: "Available"
 };
 
-const renderItemDetails = () => {
-  const details = Object.keys(newStaticItem);
+const renderFilterDetails = () => {
+  const details = Object.keys(activeFilters);
   return details.map((detail, key) => (
-    <li className="add-item-detail" key={key}>
-      <div>
-        <p>
-          {detail}: {newStaticItem[detail]}
-        </p>
-      </div>
+    <li className="add-filter-detail" key={key}>
+      <p>
+        <span className="filter-detail-name">{detail}</span>: "
+        {activeFilters[detail]}"
+      </p>
     </li>
   ));
+};
+
+const renderViewList = () => {
+  return staticViewItems.map((item, i) => {
+    return (
+      <li className="view-list-item" key={i}>
+        <h3>{item.title}</h3>
+      </li>
+    );
+  });
 };
 
 class ViewPage extends React.Component {
@@ -69,11 +74,12 @@ class ViewPage extends React.Component {
     return (
       <>
         <header role="banner">
-          <h1>your archive</h1>
+          <h1>view list</h1>
         </header>
         <section>
+          <h2>add filter:</h2>
           <form onSubmit={e => this.handleSave(e)}>
-            <div className="add-item-input-box">
+            <div className="add-filter-input-box">
               <label htmlFor="title">*title</label>
               <input
                 type="text"
@@ -83,7 +89,7 @@ class ViewPage extends React.Component {
                 required
               ></input>
             </div>
-            <div className="add-item-input-box">
+            <div className="add-filter-input-box">
               <label htmlFor="description">description</label>
               <input
                 type="text"
@@ -92,7 +98,7 @@ class ViewPage extends React.Component {
                 id="description"
               ></input>
             </div>
-            <div className="add-item-input-box">
+            <div className="add-filter-input-box">
               <label htmlFor="size">size</label>
               <input
                 type="text"
@@ -101,7 +107,7 @@ class ViewPage extends React.Component {
                 id="size"
               ></input>
             </div>
-            <div className="add-item-input-box">
+            <div className="add-filter-input-box">
               <label htmlFor="price">price</label>
               <input
                 type="number"
@@ -110,7 +116,7 @@ class ViewPage extends React.Component {
                 id="price"
               ></input>
             </div>
-            <div className="add-item-input-box">
+            <div className="add-filter-input-box">
               <label htmlFor="medium">medium</label>
               <input
                 type="text"
@@ -119,7 +125,7 @@ class ViewPage extends React.Component {
                 id="medium"
               ></input>
             </div>
-            <div className="add-item-input-box">
+            <div className="add-filter-input-box">
               <label htmlFor="location">location</label>
               <input
                 type="text"
@@ -128,7 +134,7 @@ class ViewPage extends React.Component {
                 id="location"
               ></input>
             </div>
-            <div className="add-item-input-box">
+            <div className="add-filter-input-box">
               <label htmlFor="availability">availability</label>
               <select id="availability">
                 <option value="available">available</option>
@@ -140,9 +146,9 @@ class ViewPage extends React.Component {
           </form>
         </section>
         <section>
+          <h2>active filters:</h2>
           <ul className="filter-list">
-            {renderItemDetails()}
-
+            {renderFilterDetails()}
             <li className="filter-new">
               <form className="filter-form">
                 <label htmlFor="filter-attr">+</label>
@@ -162,29 +168,17 @@ class ViewPage extends React.Component {
           </ul>
         </section>
         <section>
-          <ul className="view-list">
-            <li className="add-item-detail">
-              <div>
-                <p>filter results</p>
-              </div>
-            </li>
-            <li className="add-item-new">
-              <form className="add-item-form">
-                <label htmlFor="add-item-attr">+</label>
-                <select name="add-item-attr">
-                  <option>Add Details</option>
-                  <option>Description</option>
-                  <option>Size</option>
-                  <option>Price</option>
-                  <option>Medium</option>
-                  <option>Location</option>
-                  <option>Availablility</option>
-                  <option>Style</option>
-                  <option>Quantity</option>
-                </select>
-              </form>
-            </li>
-          </ul>
+          <h2>organize by:</h2>
+          <button>Title</button>
+          <button>Description</button>
+          <button>Size</button>
+          <button>Price</button>
+          <button>Medium</button>
+          <button>Location</button>
+          <button>Availablility</button>
+          <button>Style</button>
+          <button>Quantity</button>
+          <ul className="view-list">{renderViewList()}</ul>
         </section>
       </>
     );
