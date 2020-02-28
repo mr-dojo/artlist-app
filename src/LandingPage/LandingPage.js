0b3cb44fb2bdcf5faf22a349551faedbc929e0f5
Carlo Costantino <costantinoart@gmail.com>
2020-02-27 17:14:35 -1000

    Updated landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,42 +1,75 @@
 import React from "react";
 import "./LandingPage.css";
 
-class LandingPage extends React.Component {
-  render() {
-    return (
-      <>
-        <header role="banner">
-          <h1>Artlist</h1>
-          <h2>Helping artists organize</h2>
-        </header>
+function LandingPage(props) {
+  return (
+    <>
+      <header role="banner">
+        <h1>Artlist</h1>
+        <h2>Helping artists organize</h2>
+      </header>
 
-        <section>
-          <h3>Why did we create Artlist?</h3>
-          <p>
-            We want artists to be able to create. We want artists to easily keep
-            track of their pieces and show clients work that matches their needs
-          </p>
-        </section>
+      <section className="landing-page-section">
+        <h2>The current state of Artlist</h2>
+        <p className="landing-page-paragraph">
+          In its current state, the application is "static" meaning that the
+          functionality isn't built yet. This is a proof of concept and will be
+          used to get feedback on the idea and layout of the app.
+        </p>
+      </section>
 
-        <section>
-          <h3>What does it do for you?</h3>
-          <p>
-            Artlist allows you to see keep track of your work. It shows you,
-            at-a-glance, all of your work that is available and creates
-            customised lists to present to clients
-          </p>
-        </section>
+      <section className="landing-page-section">
+        <h3>Why did I create Artlist?</h3>
+        <p className="landing-page-paragraph">
+          As an artist, I know that it's very hard to keep my invintory in
+          order. Knowing what pieces are available and where they are located
+          can mean the difference in a sale. I've always wanted a place where I
+          could organize and keep track of my work.
+        </p>
+      </section>
 
-        <section>
-          <h3>How does it do it?</h3>
-          <p>
-            Upload works with as many details as you desire. Then use the view
-            tab to filter and create by the details you enter
-          </p>
-        </section>
-      </>
-    );
-  }
+      <section className="landing-page-section">
+        <h3>What does it do for you?</h3>
+        <p className="landing-page-paragraph">
+          Artlist helps you make customized, printable pdf lists of your
+          artwork. It shows you, at-a-glance, what you have and all available
+          information on each piece.
+        </p>
+      </section>
+
+      <section className="landing-page-section">
+        <h3>How does it do it?</h3>
+        <p className="landing-page-paragraph">
+          Create entries with as many details as you can. Then use the "view"
+          page to filter and create lists
+        </p>
+      </section>
+
+      <section className="landing-page-section">
+        <h3>Upcoming Features</h3>
+        <ul className="landing-page-features-list">
+          <li>
+            <p>- Create and export printable pdf's</p>
+          </li>
+          <li>
+            <p>- Add images to art entries</p>
+          </li>
+          <li>
+            <p>- Create an account so your list is only visible by you</p>
+          </li>
+          <li>
+            <p>- Save filter configurations to easily access specific lists</p>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>After you are finished looking around..</h3>
+        <p>Please take 5 min to fill out this survey </p>
+        <a href="https://forms.gle/tvykpNnGvvqngxXT6">-Link to survey-</a>
+      </section>
+    </>
+  );
 }
 
 export default LandingPage;
