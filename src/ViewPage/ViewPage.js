04d4f793f09176fd520a17446ac445b2fb9c5d53
Carlo Costantino <costantinoart@gmail.com>
2020-03-01 10:47:36 -1000

    Deleted static item list

--- src/ViewPage/ViewPage.js
+++ src/ViewPage/ViewPage.js
@@ -2,47 +2,6 @@ import React from "react";
 import StoreContext from "../StoreContext";
 import "./ViewPage.css";
 
-const staticViewItems = [
-  {
-    title: "Lahaina Fish",
-    medium: "oil",
-    description: "a colorful green and purple fish",
-    location: "Maui Hands Lahaina",
-    availability: "Complicated",
-    price: "2500"
-  },
-  {
-    title: "Honu",
-    medium: "mixed media",
-    description: "a turtle in a sea of blue",
-    availability: "Unavailable",
-    location: "somewhere in Canada"
-  },
-  {
-    title: "Dancing Clown",
-    medium: "acrylic",
-    description: "a scary clown",
-    location: "Maui Hands Lahaina",
-    availability: "Complicated",
-    price: "2500"
-  },
-  {
-    title: "Makawao Fish",
-    medium: "mixed media",
-    description: "A fish of the mountain",
-    availability: "Unavailable",
-    size: "20x40"
-  },
-  {
-    title: "Haleakala",
-    medium: "acrylic",
-    description: "A giant volcano that looks like a mountain",
-    size: "20x16",
-    availability: "Available",
-    price: "3000"
-  }
-];
-
 const activeFilters = {
   medium: "acrylic",
   availability: "Available"
