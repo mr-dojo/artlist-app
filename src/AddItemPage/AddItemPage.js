dafdf1c9ac0dcd9fdaf3a8fa49af783d21299530
Carlo Costantino <costantinoart@gmail.com>
2020-03-02 11:53:47 -1000

    Added handler for POST /list

--- src/AddItemPage/AddItemPage.js
+++ src/AddItemPage/AddItemPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import StoreContext from "../StoreContext";
+import { API_ENDPOINT } from "../config";
 import "./AddItemPage.css";
 
 const newStaticItem = {
@@ -22,14 +24,62 @@ const renderItemDetails = () => {
   ));
 };
 
+const handleNewItemSubmit = (e, addNewItem) => {
+  e.preventDefault();
+  const allDetails = {
+    title: e.target.title.value || null,
+    description: e.target.description.value || null,
+    medium: e.target.medium.value || null,
+    location: e.target.location.value || null,
+    price: e.target.price.value || null,
+    size: e.target.size.value || null,
+    availability: e.target.availability.value || null
+  };
+
+  const itemDetailsKeys = Object.keys(allDetails);
+
+  const deleteNullKeys = () => {
+    for (let i = 0; i < itemDetailsKeys.length; i++) {
+      if (allDetails[itemDetailsKeys[i]] === null) {
+        delete allDetails[itemDetailsKeys[i]];
+      }
+    }
+  };
+
+  deleteNullKeys();
+
+  fetch(`${API_ENDPOINT}/list`, {
+    method: "POST",
+    body: JSON.stringify(allDetails),
+    headers: {
+      "content-type": "application/json"
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        console.log(response);
+      }
+      return response.json();
+    })
+    .then(response => {
+      addNewItem(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 class AddItemPage extends React.Component {
+  static contextType = StoreContext;
+
   render() {
+    const { addNewItem } = this.context;
     return (
       <>
         <header role="banner">{renderItemTitle()}</header>
         <section>
           <h2>add details:</h2>
-          <form onSubmit={e => e.preventDefault()}>
+          <form onSubmit={e => handleNewItemSubmit(e, addNewItem)}>
             <div className="add-item-input-box">
               <label htmlFor="title">*title</label>
               <input
@@ -87,9 +137,9 @@ class AddItemPage extends React.Component {
             <div className="add-item-input-box">
               <label htmlFor="availability">availability</label>
               <select id="availability">
-                <option value="available">available</option>
-                <option value="unavailable">unavailable</option>
-                <option value="complicated">complicated</option>
+                <option value="Available">Available</option>
+                <option value="Unavailable">Unavailable</option>
+                <option value="Complicated">Complicated</option>
               </select>
             </div>
             <button type="submit">save</button>
