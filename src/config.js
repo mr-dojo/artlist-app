a1b1507697284cba61abd73a6ec279477a23bc4c
Carlo Costantino <costantinoart@gmail.com>
2020-03-09 09:43:47 -1000

    adds production api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || `http://localhost:8000`
+  //API_ENDPOINT: `http://localhost:8000`
+  API_ENDPOINT: `https://artlist-api.herokuapp.com`
 };
